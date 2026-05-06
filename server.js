9cce2d48d83d596876a7fa1d0da00230bf3ff9ba
T Naresh <150591221+06naresh@users.noreply.github.com>
2026-05-06 15:03:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ app.post('/merge', upload.array('pdfs', 12), async (req, res, next) => {
     console.log(req.files);
     let d=await mergepdfs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path));
     res.redirect(`http://localhost:3000/static/${d}.pdf`);
-    // res.send({data: req.files});
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
 })
 
 app.listen(port, () => {
